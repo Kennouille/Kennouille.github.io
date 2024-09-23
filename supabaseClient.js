77d109ab975d3544d7ec139d37e73ac4bdbad158
Kennouille <muhlemannk@gmail.com>
2024-09-23 23:48:42 +0200

    @ essai 2

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,17 +1,30 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
-        const supabaseUrl = 'https://mngggybayjooqkzbhvqy.supabase.co';
-        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZ2dneWJheWpvb3FremJodnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2MTU3NDgsImV4cCI6MjA0MjE5MTc0OH0.lnOqnq1AwN41g4xJ5O9oNIPBQqXYJkSrRhJ3osXtcsk';
-        const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = 'https://mngggybayjooqkzbhvqy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1uZ2dneWJheWpvb3FremJodnF5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY2MTU3NDgsImV4cCI6MjA0MjE5MTc0OH0.lnOqnq1AwN41g4xJ5O9oNIPBQqXYJkSrRhJ3osXtcsk';
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-        export async function insertData(data) {
-            const { error } = await supabase
-                .from('tableData')
-                .insert([data]);
-        
-            if (error) {
-                console.error('Erreur lors de l\'insertion des données :', error);
-            } else {
-                console.log('Données insérées avec succès');
-            }
-        }
+export async function insertData(data) {
+    const { error } = await supabase
+        .from('tableData')
+        .insert([data]);
+
+    if (error) {
+        console.error('Erreur lors de l\'insertion des données :', error);
+    } else {
+        console.log('Données insérées avec succès');
+    }
+}
+
+export async function fetchData() {
+    const { data, error } = await supabase
+        .from('tableData')
+        .select('*');
+
+    if (error) {
+        console.error('Erreur lors de la récupération des données :', error);
+        return [];
+    }
+
+    return data;
+}

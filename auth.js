d1233dcdcd2ae6c1c887c60f74e68b1835bc1b35
Kennouille <muhlemannk@gmail.com>
2024-10-02 21:14:25 +0200

    12

--- auth.js
+++ auth.js
@@ -3,15 +3,18 @@ const maxAttempts = 3;
 const blockDuration = 10 * 60 * 1000; // 10 minutes en millisecondes
 let isBlocked = false;
 
+function showCodePrompt() {
+    document.getElementById('code-container').style.display = 'block';
+}
+
 function checkCode() {
     if (isBlocked) {
         document.getElementById('message').innerText = "Vous êtes bloqué. Réessayez plus tard.";
         return;
     }
 
-
     const code = document.getElementById('code').value;
-    const correctCode = "109801"; // Remplacez par votre code
+    const correctCode = "votre_code_secret"; // Remplacez par votre code
 
     if (code === correctCode) {
         window.location.href = 'tableau.html';
@@ -33,8 +36,8 @@ function checkCode() {
 
 function sendEmail() {
     // Utilisez EmailJS ou un autre service pour envoyer un e-mail
-    emailjs.send("service_4ab7y3s", "template_v83qlcw", {
-        to_email: "muhlemannk@gmail.com",
+    emailjs.send("service_4ab7y3s", "template_123abc", {
+        to_email: "adresse@example.com",
         message: "L'utilisateur a été bloqué après 3 tentatives incorrectes."
     }).then(response => {
         console.log("E-mail envoyé avec succès", response.status, response.text);

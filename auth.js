4c3704e6b56873d0599d47d3acb584c11ea6abd7
Kennouille <muhlemannk@gmail.com>
2024-10-03 14:50:14 +0200

    12

--- auth.js
+++ auth.js
@@ -1,16 +1,16 @@
 let attempts = 0;
 const maxAttempts = 3;
-const blockDuration = 10 * 60 * 100; // 10 minutes en millisecondes
+const blockDuration = 10 * 60 * 1000; // 10 minutes en millisecondes
 let isBlocked = false;
 let targetPage = '';
 
 function showCodePrompt() {
-    document.getElementById('code-container').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
     targetPage = 'tableau.html';
 }
 
 function showCodePromptJournalier() {
-    document.getElementById('code-container').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
     targetPage = 'journalier.html';
 }
 
@@ -33,17 +33,15 @@ function checkCode() {
                 isBlocked = false;
                 attempts = 0;
             }, blockDuration);
-            showMessage("Vous êtes bloqué pour 1 minutes.");
+            showMessage("Vous êtes bloqué pour 10 minutes.");
         } else {
             showMessage(`Code incorrect. Tentatives restantes : ${maxAttempts - attempts}`);
         }
     }
 }
 
-
 function showMessage(message) {
     document.getElementById('message').innerText = message;
-    document.getElementById('popup').style.display = 'block';
 }
 
 function closePopup() {
